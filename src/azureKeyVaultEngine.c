083b7ae6fac98710b2e5438e47e56c6b3a473650
Christian Peter Jonigkeit <cjonigkeit@csc.com>
2017-07-10 14:40:15 +0200

    interface ready for impl

--- src/azureKeyVaultEngine.c
+++ src/azureKeyVaultEngine.c
@@ -4,6 +4,7 @@
 static const char *engine_azure_key_vault_id = "azure-keyvault";
 static const char *engine_azure_key_vault_name = "Microsoft Azure Key Vault OpenSSL engine by DXC Technologies";
 
+
 IMPLEMENT_DYNAMIC_CHECK_FN();
 IMPLEMENT_DYNAMIC_BIND_FN(bind_helper);
 
@@ -27,41 +28,57 @@ const RSA_METHOD* AZURE_KEY_VAULT_RSA_METH()
 static RSA_METHOD azure_key_vault_meth =
 {
     const char *name,
-    int (*rsa_pub_enc) (int flen, const unsigned char *from,
-                        unsigned char *to, RSA *rsa, int padding),
-    int (*rsa_pub_dec) (int flen, const unsigned char *from,
-                        unsigned char *to, RSA *rsa, int padding),
-    int (*rsa_priv_enc) (int flen, const unsigned char *from,
-                         unsigned char *to, RSA *rsa, int padding),
-    int (*rsa_priv_dec) (int flen, const unsigned char *from,
-                         unsigned char *to, RSA *rsa, int padding),
-    /* Can be null */
-    int (*rsa_mod_exp) (BIGNUM *r0, const BIGNUM *I, RSA *rsa, BN_CTX *ctx),
-    /* Can be null */
-    int (*bn_mod_exp) (BIGNUM *r, const BIGNUM *a, const BIGNUM *p,
-                       const BIGNUM *m, BN_CTX *ctx, BN_MONT_CTX *m_ctx),
-    /* called at new */
-    int (*init) (RSA *rsa),
-    /* called at free */
-    int (*finish) (RSA *rsa),
-    /* RSA_METHOD_FLAG_* things */
-    int flags,
-    /* may be needed! */
-    char *app_data,
-    /*
-     * New sign and verify functions: some libraries don't allow arbitrary
-     * data to be signed/verified: this allows them to be used. Note: for
-     * this to work the RSA_public_decrypt() and RSA_private_encrypt() should
-     * *NOT* be used RSA_sign(), RSA_verify() should be used instead. Note:
-     * for backwards compatibility this functionality is only enabled if the
-     * RSA_FLAG_SIGN_VER option is set in 'flags'.
-     */
-    int (*rsa_sign) (int type,
-                     const unsigned char *m, unsigned int m_length,
-                     unsigned char *sigret, unsigned int *siglen,
-                     const RSA *rsa),
-    int (*rsa_verify) (int dtype, const unsigned char *m,
-                       unsigned int m_length, const unsigned char *sigbuf,
-                       unsigned int siglen, const RSA *rsa),
+    azure_key_vault_rsa_pub_enc,
+    azure_key_vault_rsa_pub_dec,
+    azure_key_vault_rsa_priv_enc,
+    azure_key_vault_rsa_priv_dec,
+    NULL, // rsa_mod_exp
+    NULL, // bn_mod_exp
+    azure_key_vault_init,
+    azure_key_vault_finish,
+    0, //RSA_METHOD_FLAGS
+    NULL, // appdata
+    azure_key_vault_rsa_sign,
+    azure_key_valt_rsa_verify,
     NULL, // keygen
+}
+
+static int azure_key_vault_rsa_pub_enc(int flen, const unsigned char *from, unsigned char *to, RSA *rsa, int padding)
+{
+    return 0;
+}
+
+static int azure_key_vault_rsa_pub_dec(int flen, const unsigned char *from, unsigned char *to, RSA *rsa, int padding)
+{
+    return 0;
+}
+
+static int azure_key_vault_rsa_priv_enc(int flen, const unsigned char *from, unsigned char *to, RSA *rsa, int padding)
+{
+    return 0;
+}
+
+static int azure_key_vault_rsa_priv_dec(int flen, const unsigned char *from, unsigned char *to, RSA *rsa, int padding)
+{
+    return 0;
+}
+
+static int azure_key_vault_rsa_sign(int type, const unsigned char *m, unsigned int m_length, unsigned char *sigret, unsigned int *siglen, const RSA *rsa)
+{
+    return 0; 
+}
+
+static int azure_key_valt_rsa_verify(int dtype, const unsigned char *m, unsigned int m_length, const unsigned char *sigbuf, unsigned int siglen, const RSA *rsa)
+{
+    return 0;
+}
+
+static int azure_key_value_init(RSA *rsa)
+{
+    return 0;
+}
+
+static int azure_key_vault_finish(RSA *rsa)
+{
+    return 0;
 }
